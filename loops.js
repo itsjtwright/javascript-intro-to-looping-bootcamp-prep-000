17a7ae718071cb52b5769701048b71399a5e589a
Jacob Wright <mee@mailinator.com>
2017-12-16 23:17:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -13,10 +13,11 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(array) {
-  function maybeTrue() {
-  return Math.random() >= 0.5
-}
   do {
     array.pop();
   } while(array.lenth > 0 || maybeTrue());
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
 }
